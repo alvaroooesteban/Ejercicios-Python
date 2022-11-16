7a25b6802636d1264581361eac1c028c6b821a4d
Alvaro Esteban <alvaro.lasrosas@gmail.com>
2022-11-16 17:40:06 +0100

    jbhlj

--- Ejercicios Bucles/ej17(sueldos 2.0).py
+++ Ejercicios Bucles/ej17(sueldos 2.0).py
@@ -4,19 +4,20 @@
 #empresa por los N empleados.
 
 #datos:
-n = int(input("Ingrese la cantidad de empleados: "))
+numeroTrabajadores = int(input("Ingrese la cantidad de empleados: "))
 sueldos = []
 horas = []
+salario = int(input("Ingrese el salario por hora: "))
 
 #proceso:
-for i in range(1, n + 1):
+for i in range(1, numeroTrabajadores + 1):
     print("Ingrese las horas trabajadas por el empleado", i)
-    for j in range(1, 8):
+    for j in range(1, 7):
         horas.append(int(input("Ingrese las horas trabajadas el día " + str(j) + ": ")))
-    sueldos.append(sum(horas) * 20000)
+    sueldos.append(sum(horas) * salario)
     horas = []
 
 #salida:
-for i in range(1, n + 1):
-    print("El empleado", i, "trabajó", sueldos[i - 1] / 20000, "horas y cobró", sueldos[i - 1], "€")
+for i in range(1, numeroTrabajadores + 1):
+    print("El empleado", i, "trabajó", sueldos[i - 1] / salario, "horas y cobró", sueldos[i - 1], "€")
 print("La empresa pagó", sum(sueldos), "€")
